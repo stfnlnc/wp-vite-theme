952837dae4d93143f184f3c93e4b0fcf68dced4e
Stefan <stefan@studiokhi.com>
2025-05-26 17:03:12 +0200

    feat: menu support

--- inc/support/theme.php
+++ inc/support/theme.php
@@ -5,7 +5,8 @@ function vite_theme_supports()
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support('menus');
-    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('main', 'Menu principal');
+    register_nav_menu('secondary', 'Menu secondaire');
     register_nav_menu('footer', 'Pied de page');
     add_image_size('post-thumbnail', 1440, 900, true);
 }
